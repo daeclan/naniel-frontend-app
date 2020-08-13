8bdff66158818120601faba5da6dc877d66f7634
Declan Murphy <declxn@gmail.com>
2020-08-12 20:51:18 -0500

    init commit

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="products-index">
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+
+      <br>
+      <br>
+      <b><a v-bind:href="`/products/${product.id}`">{{product.name}}</a></b>
+      <br>
+<!-- <div class="card" style="width: 18rem;">
+  <img src=https://images-na.ssl-images-amazon.com/images/I/815W5k84jLL._AC_SL1500_.jpg class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Buy CandyLand and Give Me Money</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> -->
+  
+      <br>
+      Price: {{product.price}}
+      <br>
+      <br>
+      <i>{{product.image_url}} </i>
+      <p>∞</p>
+      </div>
+  
+  </div>
+  </div>
+</template>
+
+
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "products de naniel",
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("Hello");
+      axios.get("/api/products").then((response) => {
+        console.log(response);
+        this.products = response.data;
+      });
+    },
+  },
+};
+</script>
